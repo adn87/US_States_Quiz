409f54b6c6f05f6bc667b680da6f52ab9796945a
adn87 <aaghadieh@gmail.com>
2023-11-01 10:43:25 -0400

    US_States_Quiz_002
    - using panda module, read the csv file.
    - checked if the answer inputted by the user is in the list of states. If it is, the word would go to where the coordinates are.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import turtle
+import pandas
+
+data = pandas.read_csv("50_states.csv")
+states_List = data.state.to_list()
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+guessed_states = []
+
+while len(guessed_states) < 50:
+    user_answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's "
+                                                                                            "name?").title()
+    if user_answer in states_List:
+        guessed_states.append(user_answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == user_answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(user_answer)
+
+
 
 screen.exitonclick()
